2f471822c9e35d25232c1f7207ea25086e0e3daf
Abhishek khare <ghostabhi323@gmail.com>
2024-01-28 10:43:31 +0530

    Done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,5 @@
+"use client"
+import { Fade, Slide, Zoom } from "react-awesome-reveal"
 import About from "../../components/About"
 import Image from "next/image"
 
@@ -15,8 +17,12 @@ const page = () => {
                             <p>I have successfully defended clients facing serious charges, including <b className="text-white">Section 376(D) and 506(B) cases</b>.My advocacy extends to securing justice for victims under the <b className="text-white">ST/SC Act</b>. Notably, I have achieved positive results in <b className="text-white">NDPS cases</b>, including a significant outcome involving a substantial quantity of <b className="text-white">ganja (NDPS 20(B))</b>.In Juvenile Court proceedings and cases related to <b className="text-white">Sections 366 and 376(2)(N)</b>, my strategic approach has led to favorable outcomes for my clients.</p>
                         </div>
                         <div className="grid grid-cols-2 gap-4 mt-8">
-                            <Image width={500} height={1000} className="w-full rounded-lg" src="/nitinWithTrophy.jpg" alt="office content 1" />
-                            <Image width={500} height={1000} className="mt-4 w-full lg:mt-10 rounded-lg" src="/nitinLawyer.jpg" alt="office content 2" />
+                            <Fade>
+                                <Image width={500} height={1000} className="w-full rounded-lg" src="/nitinWithTrophy.jpg" alt="office content 1" />
+                            </Fade>
+                            <Fade delay={100}>
+                                <Image width={500} height={1000} className="mt-4 w-full lg:mt-10 rounded-lg" src="/nitinLawyer.jpg" alt="office content 2" />
+                            </Fade>
                         </div>
                     </div>
                 </section>
